9a92e10c0cceccba4553c4da0d94ac4f057c4bbf
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-09-08 17:33:35 -0400

    Add files via upload
    *Uploaded a csv file to Jupyter Notebook to display a dataframe

---
+++ WorkBook Assignment 1 part 1 (csv) .py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import pandas as pd                       #python data analysis library 
+
+Location = "Work_Book_Part_1_ISM_BI .csv" #imported data from the csv file 
+df = pd.read_csv(Location)                #read csv
+df.head ()                                # if file does not have header places a header and displays
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
