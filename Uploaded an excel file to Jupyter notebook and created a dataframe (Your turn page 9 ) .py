c55749ed8e2a243db0e8fc03e148ef2758ba8f7b
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-09-08 17:39:06 -0400

    Add files via upload
    *Uploaded an excel file to Jupyter

---
+++ Uploaded an excel file to Jupyter notebook and created a dataframe (Your turn page 9 ) .py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd 
+
+Location = "Desktop/Work_Book_Part_2_ISM_BI.xls"
+df = pd.read_excel(Location)
+df.head ()
+
+
+# In[ ]:
+
+
+
+
